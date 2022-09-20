ccba37016fdd3e1f2595ff3ae9276acc65a1252b
Mateus Marques <mateusmarquesdc@gmail.com>
2022-09-20 14:08:45 -0300

    criação da home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    // Lazy loading do componente
+    loadChildren:  () => import('./home/home.module').then(module => module.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
