5d339b3e31f15d3482f4eebc53d22f9716ac92a8
Mateus Marques <mateusmarquesdc@gmail.com>
2023-02-02 18:19:15 -0300

    feat: criando menu

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  visible: boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  openMenu() {
+    console.log('eta')
+    this.visible = !this.visible
+  }
+
+}
